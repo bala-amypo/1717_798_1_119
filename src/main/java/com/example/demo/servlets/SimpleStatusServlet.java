583d25da591b904abe37d149a47f32296a8fafb1
bala-amypo <bala@amypo.in>
2025-12-21 18:09:44 +0000

    User commit

--- src/main/java/com/example/demo/servlets/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlets/SimpleStatusServlet.java
@@ -1,22 +1,20 @@
-// package com.example.demo.servlet;
+package com.example.demo.servlet;
 
-// import jakarta.servlet.annotation.WebServlet;
-// import jakarta.servlet.http.HttpServlet;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
-// import java.io.IOException;
-// import java.io.PrintWriter;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-// @WebServlet(urlPatterns = "/status")
-// public class SimpleStatusServlet extends HttpServlet {
-    
-//     @Override
-//     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//         resp.setContentType("text/plain");
-//         resp.setStatus(200);
-        
-//         PrintWriter out = resp.getWriter();
-//         out.print("Servlet Running!");
-//         out.flush();
-//     }
-// }
+@WebServlet(urlPatterns = "/status")
+public class SimpleStatusServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setStatus(200);
+        resp.setContentType("text/plain");
+        PrintWriter out = resp.getWriter();
+        out.print("Servlet Running!");
+        out.flush();
+    }
+}
