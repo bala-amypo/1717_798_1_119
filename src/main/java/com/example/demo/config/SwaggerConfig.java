ba3264cc20f2d17febb0c67f59794d0019f8a0ed
bala-amypo <bala@amypo.in>
2025-12-22 09:05:41 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,24 +1,22 @@
 package com.example.demo.config;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
 @Configuration
-@OpenAPIDefinition(
-    info = @Info(
-        title = "Stock Portfolio Risk Analyzer API",
-        version = "1.0",
-        description = "API for analyzing stock portfolio risk",
-        contact = @Contact(
-            name = "Support",
-            email = "support@example.com"
-        ),
-        license = @License(
-            name = "Apache 2.0",
-            url = "https://www.apache.org/licenses/LICENSE-2.0.html"
-        )
-    )
-)
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9118.408procr.amypo.ai/")
+                ));
+        }
 }
+
+// //https://9005.vs.amypo.ai
