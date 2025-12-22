9f5aaa123e99c75388edbfd7c4f7dcce716fce20
bala-amypo <bala@amypo.in>
2025-12-22 08:11:42 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,33 +1,24 @@
 package com.example.demo.config;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 @OpenAPIDefinition(
     info = @Info(
         title = "Stock Portfolio Risk Analyzer API",
         version = "1.0",
-        description = "API for managing stocks and analyzing portfolio risk",
+        description = "API for analyzing stock portfolio risk",
         contact = @Contact(
-            name = "Support Team",
+            name = "Support",
             email = "support@example.com"
         ),
         license = @License(
             name = "Apache 2.0",
             url = "https://www.apache.org/licenses/LICENSE-2.0.html"
         )
-    ),
-    servers = {
-        @Server(
-            url = "http://localhost:9001",
-            description = "Local Spring Boot Server"
-        )
-    }
+    )
 )
 public class SwaggerConfig {
 }
