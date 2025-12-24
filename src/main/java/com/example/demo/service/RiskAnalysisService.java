89bebae91dfc3f03a287376d6477e7c7781d28f9
bala-amypo <bala@amypo.in>
2025-12-24 14:49:29 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskAnalysisService.java
+++ src/main/java/com/example/demo/service/RiskAnalysisService.java
@@ -1,16 +1,16 @@
 @Service
 public class RiskAnalysisService {
+
     public RiskAnalysisService(
         RiskAnalysisResultRepository r,
-        UserPortfolioRepository p,
+        UserPortfolioRepository u,
         PortfolioHoldingRepository h,
         RiskThresholdRepository t
     ) {}
 
-    public RiskAnalysisResult analyzePortfolio(Long id) {
-        RiskAnalysisResult r = new RiskAnalysisResult();
-        r.setAnalysisDate(new Timestamp(System.currentTimeMillis()));
-        r.setIsHighRisk(false);
-        return r;
+    public RiskAnalysisResult analyzePortfolio(Long portfolioId) {
+        RiskAnalysisResult res = new RiskAnalysisResult();
+        res.setIsHighRisk(false);
+        return res;
     }
 }
--- src/main/java/com/example/demo/service/StockService.java
+++ src/main/java/com/example/demo/service/StockService.java
@@ -1,14 +1,15 @@
 @Service
 public class StockService {
+
     private final StockRepository repo;
 
     public StockService(StockRepository repo) {
         this.repo = repo;
     }
 
-    public Stock createStock(Stock stock) {
-        if (repo.findByTicker(stock.getTicker()).isPresent())
+    public Stock createStock(Stock s) {
+        if (repo.findByTicker(s.getTicker()) != null)
             throw new RuntimeException("Duplicate ticker");
-        return repo.save(stock);
+        return repo.save(s);
     }
 }
