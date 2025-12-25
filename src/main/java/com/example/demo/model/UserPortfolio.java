97a766b79fc46212103dacf8df258771b1625f70
bala-amypo <bala@amypo.in>
2025-12-25 18:15:27 +0000

    User commit

--- src/main/java/com/example/demo/model/UserPortfolio.java
+++ src/main/java/com/example/demo/model/UserPortfolio.java
@@ -10,6 +10,36 @@ public class UserPortfolio {
     private Long id;
 
     private Long userId;
+    private String portfolioName;
+    private boolean active = true;
 
-    public Long getId() { return id; }
+    // ----- getters & setters -----
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getPortfolioName() {
+        return portfolioName;
+    }
+
+    public void setPortfolioName(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
