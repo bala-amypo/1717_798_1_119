2c64f06190a6e565d746c8ca5b016b25e0207937
bala-amypo <bala@amypo.in>
2025-12-25 17:02:50 +0000

    User commit

--- src/main/java/com/example/demo/model/Stock.java
+++ src/main/java/com/example/demo/model/Stock.java
@@ -3,7 +3,6 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "ticker"))
 public class Stock {
 
     @Id
@@ -15,8 +14,39 @@ public class Stock {
     private String sector;
     private Boolean active = true;
 
-    public Long getId() { return id; }
-    public String getTicker() { return ticker; }
-    public void setTicker(String ticker) { this.ticker = ticker; }
-    public Boolean getActive() { return active; }
+    public Long getId() {
+        return id;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
