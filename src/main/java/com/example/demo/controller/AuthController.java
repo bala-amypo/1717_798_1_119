3965cb7f167738304926cfce6a40c4394f898ec4
bala-amypo <bala@amypo.in>
2025-12-27 05:09:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
 import com.example.demo.security.JwtUtil;
 import com.example.demo.service.UserService;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,5 +17,19 @@ public class AuthController {
     public AuthController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
         this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        User dbUser = userService.findByEmail(user.getEmail());
+        if (dbUser == null) {
+            throw new RuntimeException("Unauthorized");
+        }
+        String token = jwtUtil.generateToken(
+                dbUser.getEmail(),
+                dbUser.getRole(),
+                1L
+        );
+        return ResponseEntity.ok(token);
     }
 }
