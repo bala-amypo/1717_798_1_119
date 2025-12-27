67001a10f4bd054018ec6cb6ac86ae013cca32cf
bala-amypo <bala@amypo.in>
2025-12-27 05:12:42 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskAnalysisResult.java
+++ src/main/java/com/example/demo/model/RiskAnalysisResult.java
@@ -1,7 +1,6 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-
 import java.sql.Timestamp;
 
 @Entity
@@ -15,12 +14,43 @@ public class RiskAnalysisResult {
     private UserPortfolio portfolio;
 
     private Timestamp analysisDate;
+
     private Double highestStockPercentage;
     private Double highestSectorPercentage;
+
     private Boolean highRisk;
 
-    public Timestamp getAnalysisDate() { return analysisDate; }
-    public void setAnalysisDate(Timestamp analysisDate) { this.analysisDate = analysisDate; }
-    public Boolean isHighRisk() { return highRisk; }
-    public void setHighRisk(Boolean highRisk) { this.highRisk = highRisk; }
+    /* ===== REQUIRED BY TESTS ===== */
+
+    public Double getHighestStockPercentage() {
+        return highestStockPercentage;
+    }
+
+    public void setHighestStockPercentage(Double highestStockPercentage) {
+        this.highestStockPercentage = highestStockPercentage;
+    }
+
+    public Double getHighestSectorPercentage() {
+        return highestSectorPercentage;
+    }
+
+    public void setHighestSectorPercentage(Double highestSectorPercentage) {
+        this.highestSectorPercentage = highestSectorPercentage;
+    }
+
+    public Boolean isHighRisk() {
+        return highRisk;
+    }
+
+    public void setHighRisk(Boolean highRisk) {
+        this.highRisk = highRisk;
+    }
+
+    public Timestamp getAnalysisDate() {
+        return analysisDate;
+    }
+
+    public void setAnalysisDate(Timestamp analysisDate) {
+        this.analysisDate = analysisDate;
+    }
 }
